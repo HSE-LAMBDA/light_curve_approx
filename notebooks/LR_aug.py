de04ac662c83bd44c7cdcdf6bc5a1e5eca890acd
BlackLik <alssval@gmail.com>
2020-12-25 01:01:25 +0300

    add polynom

--- notebooks/LR_aug.py
+++ notebooks/LR_aug.py
@@ -18,39 +18,168 @@ def create_aug_data(t_min, t_max, n_passbands, n_obs=1000):
     return np.array(t), np.array(passband)
 
 
+def arr_degree(t, log_lam, degree=1):
+    t_degree = []
+    for i in range(degree + 1):
+        t_degree.append(np.power(t, i))
+    t_degree.append(log_lam.reshape((-1,1)))
+    return t_degree
+
+
 class LinearRegressionAugmentation(object):
     def __init__(self, passband2lam):
+        """
+        Light Curve Augmentation based on FitNNRegressor
+        
+        Parameters:
+        -----------
+        passband2lam : dict
+            A dictionary, where key is a passband ID and value is Log10 of its wave length.
+            Example: 
+                passband2lam  = {0: np.log10(3751.36), 1: np.log10(4741.64), 2: np.log10(6173.23), 
+                                 3: np.log10(7501.62), 4: np.log10(8679.19), 5: np.log10(9711.53)}
+        """
         self.passband2lam = passband2lam
-        self.ss = StandardScaler()
-        self.reg = None
+        
+        self.reg      = None
+        self.X_scaler = None
+        self.y_scaler = None
+        self.ss_t     = None
+        self.poly = 7
         
     
     def fit(self, t, flux, flux_err, passband):
-        t        = np.array(t)
+        """
+        Fit an augmentation model.
+        
+        Parameters:
+        -----------
+        t : array-like
+            Timestamps of light curve observations.
+        flux : array-like
+            Flux of the light curve observations.
+        flux_err : array-like
+            Flux errors of the light curve observations.
+        passband : array-like
+            Passband IDs for each observation.
+        """
+        self.ss_t = StandardScaler().fit(np.array(t).reshape((-1, 1)))
+
+        #t_min    = t - np.array(t).min()
+        t        = self.ss_t.transform(np.array(t).reshape((-1, 1)))
+        t_square = np.power(t, 2)
+        t_cube   = np.power(t, 3)
+        t_del    = 1 / (t + 10)
+        t_exp    = np.exp(t)
+        t_exp_m  = np.exp(-t)
+        t_sin    = np.sin(t)
+        t_sinh   = np.sinh(t)
+        
         flux     = np.array(flux)
         flux_err = np.array(flux_err)
         passband = np.array(passband)
         log_lam  = add_log_lam(passband, self.passband2lam)
+        t_deg = arr_degree(t, log_lam, self.poly)
+        X = np.concatenate(t_deg, axis=1)
         
-        X = np.concatenate((t.reshape(-1, 1), log_lam.reshape(-1, 1)), axis=1)
-        X_ss = self.ss.fit_transform(X)
+        self.X_scaler = StandardScaler().fit(X)
+        X_ss = self.X_scaler.transform(X)
+        
+        self.y_scaler = StandardScaler().fit(flux.reshape((-1, 1)))
+        y_ss = self.y_scaler.transform(flux.reshape((-1, 1)))
         
         self.reg = LinearRegression()
-        self.reg.fit(X_ss, flux)
+        self.reg.fit(X_ss, y_ss)
         
     
     def predict(self, t, passband, copy=True):
-        t        = np.array(t)
+        """
+        Apply the augmentation model to the given observation mjds.
+        
+        Parameters:
+        -----------
+        t : array-like
+            Timestamps of light curve observations.
+        passband : array-like
+            Passband IDs for each observation.
+            
+        Returns:
+        --------
+        flux_pred : array-like
+            Flux of the light curve observations, approximated by the augmentation model.d
+        flux_err_pred : array-like
+            Flux errors of the light curve observations, estimated by the augmentation model.
+        """
+        t_min    = t - np.array(t).min()
+        t        = self.ss_t.transform(np.array(t).reshape((-1, 1)))
+        t_square = np.power(t, 2)
+        t_cube   = np.power(t, 3)
+        t_del    = 1 / (t + 10)
+        t_exp    = np.exp(t)
+        t_exp_m  = np.exp(-t)
+        t_sin    = np.sin(t)
+        t_sinh   = np.sinh(t)
+        
         passband = np.array(passband)
         log_lam  = add_log_lam(passband, self.passband2lam)
-        X = np.concatenate((t.reshape(-1, 1), log_lam.reshape(-1, 1)), axis=1)
-        X_ss = self.ss.transform(X)
-        flux_pred = self.reg.predict(X_ss)
-        return flux_pred
+        t_deg = arr_degree(t, log_lam, self.poly)
+        X = np.concatenate(t_deg, axis=1)
+        
+        X_ss = self.X_scaler.transform(X)
+        
+        flux_pred = self.y_scaler.inverse_transform(self.reg.predict(X_ss))
+        return np.maximum(flux_pred, 0), np.empty(flux_pred.shape)
+    
     
     def augmentation(self, t_min, t_max, n_obs=100):
+        """
+        The light curve augmentation.
+        
+        Parameters:
+        -----------
+        t_min, t_max : float
+            Min and max timestamps of light curve observations.
+        n_obs : int
+            Number of observations in each passband required.
+            
+        Returns:
+        --------
+        t_aug : array-like
+            Timestamps of light curve observations.
+        flux_aug : array-like
+            Flux of the light curve observations, approximated by the augmentation model.
+        flux_err_pred : array-like
+            Flux errors of the light curve observations, estimated by the augmentation model.
+        passband_aug : array-like
+            Passband IDs for each observation.
+        """
         t_aug, passband_aug = create_aug_data(t_min, t_max, len(self.passband2lam), n_obs)
-        flux_aug = self.predict(t_aug, passband_aug, copy=True)
-        return t_aug, flux_aug, passband_aug
+        flux_aug, flux_err_aug = self.predict(t_aug, passband_aug, copy=True)
+        return t_aug, flux_aug, flux_err_aug, passband_aug
     
+        
+    def score(self, t, flux, flux_err, passband):
+        
+        t_min    = t - np.array(t).min()
+        t        = self.ss_t.transform(np.array(t).reshape((-1, 1)))
+        t_square = np.power(t, 2)
+        t_cube   = np.power(t, 3)
+        t_del    = 1 / (t + 10)
+        t_exp    = np.exp(t)
+        t_exp_m  = np.exp(-t)
+        t_sin    = np.sin(t)
+        t_sinh   = np.sinh(t)
+        
+        flux     = np.array(flux)
+        flux_err = np.array(flux_err)
+        passband = np.array(passband)
+        log_lam  = add_log_lam(passband, self.passband2lam)
+        t_deg = arr_degree(t, log_lam, self.poly)
+        X = np.concatenate(t_deg, axis=1)
+        
+        X_ss = self.X_scaler.transform(X)
+        y_ss = self.y_scaler.transform(flux.reshape((-1, 1)))
+        
+        return self.reg.score(X_ss, y_ss)
+        
         
