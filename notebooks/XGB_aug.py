1cad1fa9bf7ae561d44c7d02126d4520c90296a1
BlackLik <alssval@gmail.com>
2021-01-28 21:16:39 +0300

    starting to learn XGB

--- notebooks/XGB_aug.py
+++ notebooks/XGB_aug.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression, Lasso, Ridge, LogisticRegression
+from xgboost import XGBRegressor
 from sklearn.preprocessing import StandardScaler
 
 
@@ -19,18 +19,10 @@ def create_aug_data(t_min, t_max, n_passbands, n_obs=1000):
     return np.array(t), np.array(passband)
 
 
-def arr_degree(t, log_lam, degree=1):
-    t_degree = []
-    for i in range(degree + 1):
-        t_degree.append(np.power(t, i))
-    t_degree.append(log_lam.reshape((-1,1)))
-    return t_degree
-
-
-class LinearRegressionAugmentation(object):
-    def __init__(self, passband2lam, mod='LR'):
+class XGBRegressionAugmentation(object):
+    def __init__(self, passband2lam):
         """
-        Light Curve Augmentation based on LinearRegression
+        Light Curve Augmentation based on XGBRegressor
         
         Parameters:
         -----------
@@ -41,13 +33,11 @@ class LinearRegressionAugmentation(object):
                                  3: np.log10(7501.62), 4: np.log10(8679.19), 5: np.log10(9711.53)}
         """
         self.passband2lam = passband2lam
-        self.mod = mod
         
         self.reg      = None
         self.X_scaler = None
         self.y_scaler = None
         self.ss_t     = None
-        self.poly = 1
         
     
     def fit(self, t, flux, flux_err, passband):
@@ -96,14 +86,8 @@ class LinearRegressionAugmentation(object):
         
         self.y_scaler = StandardScaler().fit(flux.reshape((-1, 1)))
         y_ss = self.y_scaler.transform(flux.reshape((-1, 1)))
-        if self.mod == "Lasso":
-            self.reg = Lasso(alpha=0.05)
-        elif self.mod == "Ridge":
-            self.reg = Ridge()
-        elif self.mod == "LR":
-            self.reg = LinearRegression()
-        else:
-            self.reg = LinearRegression()
+        
+        self.reg = XGBRegressor(n_estimators=100, learning_rate=0.1, max_depth=6, objective='reg:squarederror')
         self.reg.fit(X_ss, y_ss)
         
     
@@ -179,37 +163,4 @@ class LinearRegressionAugmentation(object):
         flux_aug, flux_err_aug = self.predict(t_aug, passband_aug, copy=True)
         return t_aug, flux_aug, flux_err_aug, passband_aug
     
-        
-    def score(self, t, flux, flux_err, passband):
-        
-        t_min    = t - np.array(t).min()
-        t        = self.ss_t.transform(np.array(t).reshape((-1, 1)))
-        t_square = np.power(t, 2)
-        t_cube   = np.power(t, 3)
-        t_del    = 1 / (t + 10)
-        t_exp    = np.exp(t)
-        t_exp_m  = np.exp(-t)
-        t_sin    = np.sin(t)
-        t_sinh   = np.sinh(t)
-        
-        flux     = np.array(flux)
-        flux_err = np.array(flux_err)
-        passband = np.array(passband)
-        log_lam  = add_log_lam(passband, self.passband2lam)
-        
-        X = np.concatenate((t.reshape((-1, 1)), 
-                            t_square.reshape((-1, 1)),
-                            t_cube.reshape((-1, 1)),
-                            t_del.reshape((-1, 1)),
-                            t_exp.reshape((-1, 1)),
-                            t_exp_m.reshape((-1, 1)),
-                            t_sin.reshape((-1, 1)),
-                            t_sinh.reshape((-1, 1)),
-                            log_lam.reshape((-1,1))), axis=1)
-        
-        X_ss = self.X_scaler.transform(X)
-        y_ss = self.y_scaler.transform(flux.reshape((-1, 1)))
-        
-        return self.reg.score(X_ss, y_ss)
-        
         
