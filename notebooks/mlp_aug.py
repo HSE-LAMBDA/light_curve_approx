8f3403a50e4e29f7528b293b3148f61759c4fa0c
ebsamorodova <katsam@passap.ru>
2021-05-10 21:16:09 +0300

    BNN optimizations

--- notebooks/mlp_aug.py
+++ notebooks/mlp_aug.py
@@ -1,12 +1,9 @@
 import numpy as np
 import pandas as pd
 
-import torch
-import torch.nn as nn
-from torch.utils.data import TensorDataset, DataLoader
+from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def add_log_lam(passband, passband2lam):
     log_lam = np.array([passband2lam[i] for i in passband])
@@ -20,93 +17,11 @@ def create_aug_data(t_min, t_max, n_passbands, n_obs=1000):
         passbands += [i_pb] * n_obs
     return np.array(t), np.array(passbands)
 
-
-class NNRegressor(nn.Module):
-    def __init__(self, n_inputs=1, n_hidden=10):
-        super(NNRegressor, self).__init__()
-        self.seq = nn.Sequential(
-                    nn.Linear(n_inputs, n_hidden),
-                    nn.ReLU(),
-                    nn.Linear(n_hidden, 1))
-    def forward(self, x):
-        return self.seq(x)
-    
-    
-class FitNNRegressor(object):
-    def __init__(self, n_hidden=10, n_epochs=10, batch_size=64, lr=0.01, lam=0., optimizer='Adam', debug=0):        
-        self.model = None
-        self.n_hidden = n_hidden
-        self.n_epochs = n_epochs
-        self.batch_size = batch_size
-        self.lr = lr
-        self.lam = lam
-        self.optimizer = optimizer
-        self.debug = debug
-    
-    def fit(self, X, y):
-        # Estimate model
-        self.model = NNRegressor(n_inputs=X.shape[1], n_hidden=self.n_hidden).to(device)
-        # Convert X and y into torch tensors
-        X_tensor = torch.as_tensor(X, dtype=torch.float32, device=device)
-        y_tensor = torch.as_tensor(y, dtype=torch.float32, device=device)
-        # Create dataset for trainig procedure
-        train_data = TensorDataset(X_tensor, y_tensor)
-        # Estimate loss
-        loss_func = nn.MSELoss()
-        # Estimate optimizer
-        if self.optimizer == "Adam":
-            opt = torch.optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=self.lam)
-        elif self.optimizer == "SGD":
-            opt = torch.optim.SGD(self.model.parameters(), lr=self.lr)
-        elif self.optimizer == "RMSprop":
-            opt = torch.optim.RMSprop(self.model.parameters(), lr=self.lr)
-        else:
-            opt = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-        # Enable droout
-        self.model.train(True)
-        
-        best_loss = float('inf')
-        best_state = None
-        
-        # Start the model fit
-        for epoch_i in range(self.n_epochs):
-            loss_history = []
-            for x_batch, y_batch in DataLoader(train_data, batch_size=self.batch_size, shuffle=True):
-                # make prediction on a batch
-                y_pred_batch = self.model(x_batch)
-                loss = loss_func(y_batch, y_pred_batch)
-                # zero the parameter gradients
-                for param in self.model.parameters():
-                    param.grad = None
-
-                # backpropagate gradients
-                loss.backward()
-                # update the model weights
-                opt.step()
-                loss_history.append(loss.item())
-            if self.debug:
-                print("epoch: %i, mean loss: %.5f" % (epoch_i, np.mean(loss_history)))
-            if np.mean(loss_history) <= best_loss:
-                best_loss = np.mean(loss_history)
-                best_state = self.model.state_dict()
-        self.model.load_state_dict(best_state)
-    
-    def predict(self, X):
-        with torch.no_grad():
-            # Disable droout
-            self.model.train(False)
-            # Convert X and y into torch tensors
-            X_tensor = torch.as_tensor(X, dtype=torch.float32, device=device)
-            # Make predictions for X 
-            y_pred = self.model(X_tensor)
-            y_pred = y_pred.cpu().detach().numpy()
-            return y_pred
-    
-class SingleLayerNetAugmentation(object):
+class MLPRegressionAugmentation(object):
     
     def __init__(self, passband2lam):
         """
-        Light Curve Augmentation based on NNRegressor with new features
+        Light Curve Augmentation based on Gaussian Processes Regression
         
         Parameters:
         -----------
@@ -116,14 +31,14 @@ class SingleLayerNetAugmentation(object):
                 passband2lam  = {0: np.log10(3751.36), 1: np.log10(4741.64), 2: np.log10(6173.23), 
                                  3: np.log10(7501.62), 4: np.log10(8679.19), 5: np.log10(9711.53)}
         """
-       
+        
         self.passband2lam = passband2lam
-
+        
         self.ss_x = None
         self.ss_y = None
         self.ss_t = None
         self.reg = None
-
+    
     def get_features(self, t, passband, ss_t):
         passband = np.array(passband)
         log_lam  = add_log_lam(passband, self.passband2lam)
@@ -140,7 +55,7 @@ class SingleLayerNetAugmentation(object):
         Parameters:
         -----------
         t : array-like
-            Scaled timestamps of light curve observations.
+            Timestamps of light curve observations.
         flux : array-like
             Flux of the light curve observations.
         flux_err : array-like
@@ -158,8 +73,10 @@ class SingleLayerNetAugmentation(object):
         
         self.ss_y = StandardScaler().fit(flux.reshape((-1, 1)))
         y_ss = self.ss_y.transform(flux.reshape((-1, 1)))
-        self.reg = FitNNRegressor(n_hidden=80, n_epochs=100, batch_size=2, optimizer='SGD')
-        self.reg.fit(X_ss, y_ss)
+
+        self.reg = MLPRegressor(hidden_layer_sizes=(20,10,), solver='lbfgs', activation='tanh', learning_rate_init=0.001,
+                               max_iter=90, batch_size=1)
+        self.reg.fit(X_ss, y_ss.reshape((1, -1))[0])
     
     
     def predict(self, t, passband, copy=True):
@@ -169,7 +86,7 @@ class SingleLayerNetAugmentation(object):
         Parameters:
         -----------
         t : array-like
-            Scaled timestamps of light curve observations.
+            Timestamps of light curve observations.
         passband : array-like
             Passband IDs for each observation.
             
@@ -180,7 +97,7 @@ class SingleLayerNetAugmentation(object):
         flux_err_pred : array-like
             Flux errors of the light curve observations, estimated by the augmentation model.
         """
-
+        
         X = self.get_features(t, passband, self.ss_t)
         X_ss = self.ss_x.transform(X)
         
